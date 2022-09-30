84fd1f31bc38962ace6cb850d88fecb36158672d
sneha2395 <agarwal.sneha.231995@gmail.com>
2022-09-30 19:50:48 +0530

    learned sorting and occurence

--- arraysum.js
+++ arraysum.js
@@ -1,9 +1,25 @@
 const arr = [1, 4, 7, 8, 4, 9, 7, 3, 5, 8, 1, 26, 99, 26, 24, 18, 17, 19, 23];
 let sum = 0;
+let result = {};
 
-//for loop
+const itemcounter=(arr, item) => {
+    let counter = 0;
+}
+
+//for sum
 for(let i = 0; i < arr.length; i++){
     sum += arr[i];
 }
+ //for occurence
+ for(let i = 0; i < arr.length; i++){
+    if(arr[i] in result){
+       result [arr[i]] = result [arr[i]]+1
+    }
+    else{
+        result[arr[i]] = 1;
+    }
+ }
 
-console.log(sum);
+ arr.sort((a, b) => a - b );
+
+console.log("sorted array", arr);
